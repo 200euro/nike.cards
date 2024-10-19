b3822e251c50aa29b17a830a9bc1248dc22f0127
200euro <AKMALKADYROV281@GMAIL.COM>
2024-10-19 17:09:26 +0500

    Add files via upload

---
+++ src/components/card/Card2.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card2 = () => {
+  return (
+    <div className="card card-2">
+      <img
+        src="https://dznike-githubuzer1237s-projects.vercel.app/nike-blue.png"
+        alt=""
+      />
+      <h2>Nike X OFF-White</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <h3>$1599</h3>
+      <div>
+        <button>Buy Now</button>
+        <button>⭐</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card2;
